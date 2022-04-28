352e8487076781823d80be1e7c091f2bec700de7
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-28 08:54:27 +0530

    [Ronit]Added : Harmonic Number

---
+++ Harmonic Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonic_Number
+{
+    internal class Program
+    {
+        public static void HarmonicNumber()
+        {
+            Console.WriteLine("Welcome to Harmonic Number Program");
+            Console.WriteLine("Please enter the range : ");
+            int range = Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+            for (int i = 1; i < range; i++)
+            {
+                result = (1/(Convert.ToDouble(i)));
+                if(i == range)
+                {
+                    Console.WriteLine($"1/{i} = ");
+                }
+                else
+                {
+                    Console.WriteLine($"1/{i} + ");
+                }
+            }
+            Console.WriteLine("The sum is = " + result);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Program.HarmonicNumber();
+        }
+    }
+}
